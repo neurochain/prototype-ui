bfeee31f26aa2b04e7c6a87ccd3d9c43b1b8ed9a
NeuroChain Dev <neurochaintech@gmail.com>
2018-03-12 22:44:12 +0100

    Prepare UI for front

--- launcher.js
+++ launcher.js
@@ -2,8 +2,10 @@ var childProcess = require('child_process');
 
 var config = JSON.parse(require('fs').readFileSync('./config/'+(process.argv[2] ? process.argv[2] : 'default')+'.json'));
 
-config.forEach(
-  (bot) => {
-    childProcess.fork('./node_modules/NeuroChainBot/server.js', [bot.id, bot.listeningPort, bot.sendingPort, bot.seed.host, bot.seed.port, bot.connectorUDPPort, bot.websocketPort]);
-  }
-);
+var i = 0;
+var interval = setInterval(function(){
+    var bot = config[i];
+    console.log('Starting bot '+ bot.id);
+    childProcess.fork('./node_modules/NeuroChainBot/server.js', [bot.id, bot.listeningPort, bot.sendingPort, bot.seed.host, bot.seed.port, bot.connectorUDPPort, bot.websocketPort, bot.dbSync == true]);
+    if(++i === config.length) clearInterval(interval);
+}, 2000);
